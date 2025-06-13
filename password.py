9e74b2fec587796b747d15d25e93417480f7657a
Yout Name <youtube.com>
2025-06-13 19:16:36 +0900

    아이디 기준 비번 찾기

--- password.py
+++ password.py
@@ -22,3 +22,12 @@ def save_user(user_id, encrypted_pw):
     with open(user_db, "a") as f:
         f.write(f"{user_id} {encrypted_pw.decode()}\n")
 
+def find_user(user_id):
+    if not os.path.exists(user_db):
+        return None
+    with open(user_db, "r") as f:
+        for line in f:
+            stored_id, stored_pw = line.strip().split()
+            if stored_id == user_id:
+                return stored_pw.encode()
+    return None
