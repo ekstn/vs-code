b728bbd04756a215d75b8884848f38d696d11d17
Yout Name <youtube.com>
2025-06-13 19:12:30 +0900

    키 불러오기

--- password.py
+++ password.py
@@ -7,3 +7,13 @@ from cryptography.fernet import Fernet
 user_db = "users.txt"
 
 key = "secret.key"
+
+def load_key():
+    if os.path.exists(key):
+        with open(key, "rb") as f:
+            return f.read()
+    else:
+        key = Fernet.generate_key()
+        with open(key, "wb") as f:
+            f.write(key)
+        return key
